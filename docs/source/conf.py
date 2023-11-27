72e9e8662c5262f6df744fe0473819b87dc33e3a
Hui Wan <76040122+aster-ww@users.noreply.github.com>
2023-11-27 23:09:13 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,10 @@ import inspect
 import sys
 from datetime import date
 from pathlib import Path
-
+import mock   
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate'] 
+for mod_name in MOCK_MODULES: 
+    sys.modules[mod_name] = mock.Mock()
 # import git
 
 sys.path.insert(0, str(Path("../..").absolute()))
