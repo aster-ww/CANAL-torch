511e6452bae8ed1eb650f4aed37255ace7293705
Hui Wan <76040122+aster-ww@users.noreply.github.com>
2023-11-28 00:18:43 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys
 from datetime import date
 from pathlib import Path
 import mock   
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','torch','pandas','scikit-learn'] 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','torch','pandas','scikit-learn','anndata'] 
 for mod_name in MOCK_MODULES: 
     sys.modules[mod_name] = mock.Mock()
 # import git
